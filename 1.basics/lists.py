52c0349165b17f347637727f2f3331a7f220e388
robjacobo <robjacobox@gmail.com>
2024-11-18 16:36:21 -0600

    tuples started

--- 1.basics/lists.py
+++ 1.basics/lists.py
@@ -3,3 +3,36 @@ x = ["one", "two"]
 
 print(x)
 print(type(x))
+
+lista_cursos = ['Python', 'Django', 'Flask', 'Ruby', 'Java']
+
+lista_cursos[4] = 'Rust'
+
+print(lista_cursos)
+
+# Sublistas
+
+sub_lista = lista_cursos[0:3]
+print(sub_lista)
+sub_lista = lista_cursos[2:]
+print(f"[2:] {sub_lista}")
+sub_lista = lista_cursos[:3]
+print(f"[:3] {sub_lista}")
+
+sub_lista = lista_cursos[1:5:2]
+print(f"[1:5:2] {sub_lista}")
+
+sub_lista = lista_cursos[::2]
+print(f"[::2] {sub_lista}")
+
+
+lista_cursos.append("JavaScript")
+lista_cursos.insert(1, "C")
+print(lista_cursos)
+
+nueva_lista = ["Docker", "C++", "MongoDB"]
+lista_cursos.extend(nueva_lista)
+print(lista_cursos)
+
+lista_cursos.remove("MongoDB")
+print(lista_cursos);
---
+++ 1.basics/tuples.py
@@ -0,0 +1,4 @@
+tupla = ('Roberto', 24, True, [1, 2, 3], (4, 5, 6))
+print(tupla)
+
+print(tupla[0])
