c773190d5df33242eee1fe57302680b4d45f62d0
roberjacobo <robjacobox@gmail.com>
2024-12-01 12:59:02 -0600

    tuples added

--- 1.basics/tuples.py
+++ 1.basics/tuples.py
@@ -2,3 +2,29 @@ tupla = ('Roberto', 24, True, [1, 2, 3], (4, 5, 6))
 print(tupla)
 
 print(tupla[0])
+
+
+# Descomprimir
+# * -> Lista
+# _ -> Omitir valor(es)
+
+numeros = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+uno, dos, tres, cuatro, *resto_valores = numeros
+
+print(uno)
+print(dos)
+print(tres)
+print(cuatro)
+print(resto_valores)
+
+
+# *_ -> Va a ignorar los valores que abarque
+uno, dos, tres, cuatro, *_, diez = numeros
+
+
+print(uno)
+print(dos)
+print(tres)
+print(cuatro)
+print(diez)
